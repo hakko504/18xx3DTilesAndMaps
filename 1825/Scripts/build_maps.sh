c218cdf3820a07782c4f0a4e7e138c89e0f1449f
Håkan König <github@hakko.eml.cc>
2022-09-06 13:29:01 +0200

    build a specific game

--- 1825/Scripts/build_maps.sh
+++ 1825/Scripts/build_maps.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
+
+build_units() {
+  find . -type f -name '1825 U*.scad' | xargs -t -I % openscad "%" -o "%.stl"
+}
+build_regions() {
+  find . -type f -name '1825 R*.scad' | xargs -t -I % openscad "%" -o "%.stl"
+}
+
 echo "Building 1825 Maps"
 cd ../Maps
-for i in *.scad; do
-  echo "Building $i"
-  openscad "$i" -o "${i%.scad}.stl"
+openscad '1825 Full Map.scad' -o '1825 Full Map.stl' &
+build_units &
+build_regions &
+wait
+for i in *.scad.stl ; do
+  mv "$i" "${i%.scad.stl}.stl"
 done
+
