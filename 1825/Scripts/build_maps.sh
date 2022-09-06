2ec4a4d122e6122af3824f83ce7a1eb020d579b5
Håkan König <github@hakko.eml.cc>
2022-09-06 13:29:55 +0200

    build full map in parallell with parts

--- 1825/Scripts/build_maps.sh
+++ 1825/Scripts/build_maps.sh
@@ -1,17 +1,32 @@
 #!/bin/bash
 
-build_units() {
-  find . -type f -name '1825 U*.scad' | xargs -t -I % openscad "%" -o "%.stl"
+build_whole_map() {
+  openscad '1825 Full Map.scad' -o '1825 Full Map.stl'
 }
+
+build_u1() {
+  find . -type f -name '1825 U1*.scad' | xargs -t -I % openscad "%" -o "%.stl"
+}
+build_u2() {
+  find . -type f -name '1825 U2*.scad' | xargs -t -I % openscad "%" -o "%.stl"
+}
+build_u3() {
+  find . -type f -name '1825 U3*.scad' | xargs -t -I % openscad "%" -o "%.stl"
+}
+
 build_regions() {
   find . -type f -name '1825 R*.scad' | xargs -t -I % openscad "%" -o "%.stl"
 }
 
+
+
 echo "Building 1825 Maps"
 cd ../Maps
-openscad '1825 Full Map.scad' -o '1825 Full Map.stl' &
-build_units &
-build_regions &
+build_whole_map &
+build_u1
+build_u2
+build_u3
+build_regions
 wait
 for i in *.scad.stl ; do
   mv "$i" "${i%.scad.stl}.stl"
